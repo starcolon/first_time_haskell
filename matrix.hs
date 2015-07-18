bb9b3b24d1345f1b4578a83cba19a076275990ad
starcolon <tao@starcolon.com>
2015-07-18 20:47:52 +0700

    IMPLEMENT MATRIX SCALE FUNCTION

--- matrix.hs
+++ matrix.hs
@@ -1,12 +1,25 @@
 module Main where
 
-
-
 identity :: Int -> [[Int]]
 identity n 
 	| n<=1 = [[1]]
 	| otherwise = [[if w==v then 1 else 0|w <- [0..n-1]]|v <- [0..n-1]]
 
+
+addrow :: [[Int]] -> [Int] -> [[Int]]
+addrow [[]] [w] 		= [[w]]
+addrow [ws] [v] 		= [ws++[v]] 
+
+
+scalerow :: Int -> [Int] -> [Int]
+scalerow _ []			= []
+scalerow c (w:[])		= [w*c]
+scalerow c (w:ws)		= [w*c] ++ scalerow c ws
+
+
+scale :: Int -> [[Int]] -> [[Int]]
+scale _ [[]]			= [[]]
+scale c (w:ws)			= [scalerow c w] ++ scale c ws
 
 
 printmat :: [[Int]] -> IO()
@@ -17,8 +30,10 @@ printmat ws = print (head ws) >> printmat (tail ws)
 
 
 i9 = identity 9
+c9 = scale 25 i9
 
 main :: IO()
 main = do
 	putStr "------------------\n"
 	printmat i9
+	printmat c9
